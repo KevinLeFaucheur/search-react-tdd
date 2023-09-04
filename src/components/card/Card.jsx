823d0e9640e513ae12dba74dceb353d083af29f3
Kevin Le Faucheur <lefaucheur.ke@gmail.com>
2023-09-04 20:43:30 +0200

    Styling cards part 1

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,12 +8,17 @@ export const Card = ({ movie }) => {
 
       </Head>
       <Body>
-        <div>{movie.title}</div>
-        <div>{movie.genre}</div>
-        <div>{movie.time}</div>
-        <div>{movie.city}</div>
-        <div>{movie.director}</div>
-        <div>{movie.language}</div>
+        <Title>{movie.title}</Title>
+        <CardHeader>
+          <Director>{movie.director}</Director>
+          <Genre>{movie.genre}</Genre>
+        </CardHeader>
+        <section>
+          <City>{movie.city}</City>
+          <Time>{movie.time}</Time>
+          <Language>{movie.language}</Language>
+          <Price>{movie.price}</Price>
+        </section>
       </Body>
     </Container>
   )
@@ -23,16 +28,49 @@ const Container = styled.div`
   width: 30%;
   height: 400px;
   background-color: #E7E7E7;
-  border-radius: 5px;
+  border-radius: 15px;
   margin-bottom: 1rem;
 `
 
 const Head = styled.header`
   height: 65%;
   background-color: #929292;
-  border-radius: 5px 5px 0 0;
+  border-radius: 15px 15px 0 0;
 `
 
 const Body = styled.div`
+  padding: 10px;
+`
+
+const CardHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const Title = styled.div`
+
+`
+
+const Director = styled.div`
+
+`
+
+const Genre = styled.div`
+
+`
+
+const City = styled.div`
+
+`
+
+const Time = styled.div`
+
+`
+
+const Price = styled.div`
+
+`
+
+const Language = styled.div`
 
 `
